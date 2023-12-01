d08da37984ab3d760803fc67f8f4be6dffb56b1d
Øyvind Mathisen <OyvindMathisenWork@gmail.com>
2023-12-01 10:39:03 +0100

    Test to allow opening on the top frame

--- iframe-app.js
+++ iframe-app.js
@@ -3,3 +3,9 @@ function launch() {
     const input = inputElement.value;
     window.location.assign(input);
 }
+
+function launchTop() {
+    const inputElement = document.getElementById("launch-link-input");
+    const input = inputElement.value;
+    window.top.location.assign(input);
+}
