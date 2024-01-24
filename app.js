309222d43d53045b84961dc0dfc9530bb7895b8c
Øyvind Mathisen <OyvindMathisenWork@gmail.com>
2024-01-24 08:34:55 +0100

    FIX: ClientScript not always opening due to script not being loaded yet

--- app.js
+++ app.js
@@ -5,21 +5,34 @@ function openPaymentMenu() {
     script.src = scriptUrl;
     script.type = "text/javascript";
     script.id = "payex-payment-menu-script";
-    document.body.insertAdjacentElement("afterbegin", script);
-
-    setTimeout(() => {
+    script.onload = function () {
         const config = {
-            onPaymentAttemptStarted: Function = (data) => console.log(`onPaymentAttemptStarted: ${JSON.stringify(data)}`),
-            onPaymentAttemptFailed: Function = (data) => console.log(`onPaymentAttemptFailed: ${JSON.stringify(data)}`),
-            onPaymentAttemptAborted: Function = (data) => console.log(`onPaymentAttemptAborted: ${JSON.stringify(data)}`),
-            onPaymentTransactionFailed: Function = (data) => console.log(`onPaymentTransactionFailed: ${JSON.stringify(data)}`),
-            onOutOfViewRedirect: Function = (data) => console.log(`onOutOfViewRedirect: ${JSON.stringify(data)}`),
-            onPaid: Function = (data) => console.log(`onPaid: ${JSON.stringify(data)}`),
-            onOutOfViewOpen: Function = (data) => console.log(`onOutOfViewOpen: ${JSON.stringify(data)}`),
-            onPaymentCreated: Function = (data) => console.log(`onPaymentCreated: ${JSON.stringify(data)}`),
-            // onExternalRedirect: Function = (data) => console.log(`onExternalRedirect: ${JSON.stringify(data)}`),
-            onPaymentCompleted: Function = (data) => console.log(`onPaymentCompleted: ${JSON.stringify(data)}`),
-            onError: Function = (data) => console.log(`onError: ${JSON.stringify(data)}`),
+            onPaymentAttemptStarted: Function = (data) => handleEvent("onPaymentAttemptStarted", data),
+            onPaymentAttemptFailed: Function = (data) => handleEvent("onPaymentAttemptFailed", data),
+            onPaymentAttemptAborted: Function = (data) => handleEvent("onPaymentAttemptAborted", data),
+            onPaymentTransactionFailed: Function = (data) => handleEvent("onPaymentTransactionFailed", data),
+            onOutOfViewRedirect: Function = (data) => handleEvent("onOutOfViewRedirect", data),
+            onPaid: Function = (data) => handleEvent("onPaid", data),
+            onOutOfViewOpen: Function = (data) => handleEvent("onOutOfViewOpen", data),
+            onPaymentCreated: Function = (data) => handleEvent("onPaymentCreated", data),
+            onExternalRedirect: Function = (data) => handleEvent("onExternalRedirect", data),
+            onPaymentCompleted: Function = (data) => handleEvent("onPaymentCompleted", data),
+            onError: Function = (data) => handleEvent("onError", data),
+
+            onEventNotification: Function = (data) => handleEvent("onEventNotification", data),
+            onInstrumentSelected: Function = (data) => handleEvent("onInstrumentSelected", data),
+            onTermsOfServiceRequested: Function = (data) => handleEvent("onTermsOfServiceRequested", data),
+            onFailed: Function = (data) => handleEvent("onFailed", data),
+            onAborted: Function = (data) => handleEvent("onAborted", data),
+            onCheckoutResized: Function = (data) => handleEvent("onCheckoutResized", data),
+            onCheckoutLoaded: Function = (data) => handleEvent("onCheckoutLoaded", data),
+            onApplicationConfigured: Function = (data) => handleEvent("onApplicationConfigured", data),
+            onPaymentMenuInstrumentSelected: Function = (data) => handleEvent("onPaymentMenuInstrumentSelected", data),
+            onPaymentToS: Function = (data) => handleEvent("onPaymentToS", data),
+            onPaymentFailed: Function = (data) => handleEvent("onPaymentFailed", data),
+            onPaymentCanceled: Function = (data) => handleEvent("onPaymentCanceled", data),
+            onPaymentPending: Function = (data) => handleEvent("onPaymentPending", data),
+
             container: any = "paymentmenu-container",
             culture: string = "en-us"
         };
@@ -32,7 +45,17 @@ function openPaymentMenu() {
             window.payex.hostedView.vipps(config).open();
         else if (scriptUrl.includes("invoice"))
             window.payex.hostedView.invoice(config).open();
+        else if (scriptUrl.includes("creditcard"))
+            window.payex.hostedView.creditCard(config).open();
         else
             window.payex.hostedView.checkout(config).open();
-    }, 1000);
+    }
+    document.body.insertAdjacentElement("afterbegin", script);
+}
+
+function handleEvent(event, data) {
+    console.log(`${event}: ${JSON.stringify(data)}`);
+
+    const textField = document.getElementById("debug-logger");
+    textField.value += `${JSON.stringify(data)}\n`;
 }
