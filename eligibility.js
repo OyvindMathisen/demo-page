7b504d99719d76b40813f08211f2ac160eadc2bb
Øyvind Mathisen <OyvindMathisenWork@gmail.com>
2024-03-15 15:29:07 +0100

    FEATURE: Added the UserAgent used on the page for further debugging

--- eligibility.js
+++ eligibility.js
@@ -1,8 +1,13 @@
+window.addEventListener("load", (_) => {
+    displayUserAgent();
+});
+
 async function checkEligibility() {
+    displayUserAgent();
+    
     const acceptedWallets = await payex.getAcceptedWallets();
     console.log("Available instruments: " + acceptedWallets.join(", "));
 
-    // The following code is just to visually display the instruments in the HTML document
     const container = document.getElementById("container");
     container.textContent = "";
     
@@ -11,4 +16,13 @@ async function checkEligibility() {
         paragraph.innerHTML = element;
         container.appendChild(paragraph);
     });
+}
+
+function displayUserAgent() {
+    const userAgentContainer = document.getElementById("userAgent");
+    userAgentContainer.textContent = "";
+
+    const paragraph = document.createElement("p");
+    paragraph.innerHTML = navigator.userAgent;
+    userAgentContainer.appendChild(paragraph);
 }
