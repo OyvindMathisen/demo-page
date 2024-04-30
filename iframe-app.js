c2506ad8c06d54003188f01f916f3a0a9a0bb245
Øyvind Mathisen <OyvindMathisenWork@gmail.com>
2024-04-30 13:44:23 +0200

    FEATURE: Expanded iFrame launch buttons to support multiple methods (based on Vitaliia's switch case for BankID successful launch)

--- iframe-app.js
+++ iframe-app.js
@@ -1,11 +1,23 @@
-function launch() {
+function launchLink(method) {
     const inputElement = document.getElementById("launch-link-input");
     const input = inputElement.value;
-    window.location.assign(input);
-}
-
-function launchTop() {
-    const inputElement = document.getElementById("launch-link-input");
-    const input = inputElement.value;
-    window.top.location.assign(input);
+    switch (method) {
+        case "window_location_href":
+            window.location.href = input;
+            break;
+        case "window_open":
+            window.open(input, "_blank", "noopener,noreferrer");
+            break;
+        case "document_location":
+            this.document.location.href = input;
+            break;
+        case "location_assign":
+            window.location.assign(input);
+            break;
+        case "location_top_assign":
+            window.top.location.assign(input);
+            break;
+        default:
+            console.warn(`Unknown launch method '${method}'`);
+    }
 }
